e00b19add45535d3d8ad653306322cf1e448c06c
Kausstubh Jaiswal <kausstubhjaiswal91256@gmail.com>
2024-05-14 13:34:30 +0530

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './Search.css';
-require('dotenv').config();
 
 const Search = () => {
   const [searchParams, setSearchParams] = useState({
